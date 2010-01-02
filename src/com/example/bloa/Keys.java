874d1fe6827ff20d91ae30a2647e4c1121fe2563
unknown <Brion Emde@.(none)>
2010-01-02 11:32:23 -0700

    added a stub Keys.java

---
+++ src/com/example/bloa/Keys.java
@@ -0,0 +1,6 @@
+package com.example.bloa;
+
+public class Keys {
+	public static final String TWITTER_CONSUMER_KEY = "Your Application Token from Twitter";
+	public static final String TWITTER_CONSUMER_SECRET = "Your Application Secret from Twitter";
+}
