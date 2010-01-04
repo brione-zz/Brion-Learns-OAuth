729fd40d78342755b10cb2a2fb0f1bcf33e594d8
unknown <Brion Emde@.(none)>
2010-01-04 14:30:47 -0700

    converted the http stuff to use AsyncTask and progress dialogs

--- src/com/example/bloa/BLOA.java
+++ src/com/example/bloa/BLOA.java
@@ -33,9 +33,11 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -45,114 +47,102 @@ import android.widget.CheckBox;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.widget.CompoundButton.OnCheckedChangeListener;
 
-public class BLOA extends Activity implements OnCheckedChangeListener, OnClickListener {
+public class BLOA extends Activity implements OnCheckedChangeListener,
+		OnClickListener {
 	public static final String TAG = "BLOA";
 
 	public static final String TWITTER_REQUEST_TOKEN_URL = "http://twitter.com/oauth/request_token";
 	public static final String TWITTER_ACCESS_TOKEN_URL = "http://twitter.com/oauth/access_token";
 	public static final String TWITTER_AUTHORIZE_URL = "http://twitter.com/oauth/authorize";
-	
+
 	private static final Uri CALLBACK_URI = Uri.parse("bloa-app://twitt");
-	
+
 	private static final String PREFS = "MyPrefsFile";
-	
+
 	private OAuthConsumer mConsumer;
 	private OAuthProvider mProvider;
-	
-	private DefaultHttpClient mClient;
-	
+
 	private CheckBox mCB;
 	private EditText mEditor;
 	private Button mButton;
 	private TextView mDisplay;
 	private TextView mUser;
 	
+	ProgressDialog postDialog = null;
+
 	private static final String TOKEN_STRING = "token";
 	private static final String SECRET_STRING = "secret";
-	
+
 	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        mCB = (CheckBox) this.findViewById(R.id.enable);
-        mCB.setChecked(false);
-        mEditor = (EditText) this.findViewById(R.id.editor);
-        mButton = (Button) this.findViewById(R.id.post);
-        mDisplay = (TextView) this.findViewById(R.id.last);
-        mUser = (TextView) this.findViewById(R.id.user);
-        
-        if(savedInstanceState != null) {
-        	mConsumer = (OAuthConsumer) savedInstanceState.getSerializable("consumer");
-        	mProvider = (OAuthProvider) savedInstanceState.getSerializable("provider");
-        	String token = mConsumer.getToken();
-        	String secret = mConsumer.getTokenSecret();
-        	if(token != null && secret != null) {
-	 	    	Log.d(TAG, "From Saved!! Token: " + token + ", Secret: " + secret);
-        		// mCB.setChecked(true);
-        		// mEditGroup.setEnabled(true);
-        	}
-        } else {
-	        mConsumer = new CommonsHttpOAuthConsumer(
-	        		Keys.TWITTER_CONSUMER_KEY,
-	        		Keys.TWITTER_CONSUMER_SECRET,
-	        		SignatureMethod.HMAC_SHA1);
-	 	    
-	        SharedPreferences settings = this.getSharedPreferences(PREFS, 0);
-	 	    if(settings.contains(TOKEN_STRING) && settings.contains(SECRET_STRING)) {
-	 	    	String token = settings.getString(TOKEN_STRING, "");
-	 	    	String secret = settings.getString(SECRET_STRING, "");
-	 	    	Log.d(TAG, "From preferences!! Token: " + token + ", Secret: " + secret);
-	 	    	mConsumer.setTokenWithSecret(token, secret);
-	 	    }
-			mProvider = new DefaultOAuthProvider(mConsumer,TWITTER_REQUEST_TOKEN_URL,TWITTER_ACCESS_TOKEN_URL,TWITTER_AUTHORIZE_URL);
-        }
-        mClient = new DefaultHttpClient();
-        JSONObject jso;
-        if((jso = getCredentials()) != null) {
-        	mButton.setEnabled(true);
-        	mEditor.setEnabled(true);
-    		mCB.setChecked(true);
-        	mUser.setText(jso.optString("name", "Bad Value"));
-        }
-        mButton.setOnClickListener(this);
-        mCB.setOnCheckedChangeListener(this);
-    }
-    
-    @Override
-    protected void onSaveInstanceState(Bundle b) {
-    	b.putSerializable("provider", mProvider);
-    	b.putSerializable("consumer", mConsumer);
-    }
-    
-    @Override
-    protected void onNewIntent(Intent i) {
-    	
-    	Uri uri = i.getData();
-    	if (uri != null && CALLBACK_URI.getScheme().equals(uri.getScheme())) {
-    	    try {
-        	    String verifier = uri.getQueryParameter(OAuth.OAUTH_VERIFIER);
-        	    mProvider.retrieveAccessToken(verifier);
-        	    String token = mConsumer.getToken();
-        	    String secret = mConsumer.getTokenSecret();
-	 	    	Log.d(TAG, "New OAuth Token: " + token + ", New Secret: " + secret);
-	 	    	JSONObject jso;
-	 	        if((jso = getCredentials()) != null) {
-	 	        	mButton.setEnabled(true);
-	 	        	mEditor.setEnabled(true);
-	         	    SharedPreferences settings = this.getSharedPreferences(PREFS, 0);
-	        	    SharedPreferences.Editor editor = settings.edit();
-	        	    if(!settings.contains(TOKEN_STRING))
-	        	    		editor.putString(TOKEN_STRING, token);
-	        	    if(!settings.contains(SECRET_STRING))
-	        	    	editor.putString(SECRET_STRING, secret);
-	        	    editor.commit();
-	        	    
-	        	    mUser.setText(jso.optString("name", "Bad Value"));
-	 	        }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		mCB = (CheckBox) this.findViewById(R.id.enable);
+		mCB.setChecked(false);
+		mEditor = (EditText) this.findViewById(R.id.editor);
+		mButton = (Button) this.findViewById(R.id.post);
+		mDisplay = (TextView) this.findViewById(R.id.last);
+		mUser = (TextView) this.findViewById(R.id.user);
+
+		if (savedInstanceState != null) {
+			mConsumer = (OAuthConsumer) savedInstanceState
+					.getSerializable("consumer");
+			mProvider = (OAuthProvider) savedInstanceState
+					.getSerializable("provider");
+			String token = mConsumer.getToken();
+			String secret = mConsumer.getTokenSecret();
+			if (token != null && secret != null) {
+				Log.d(TAG, "From Saved!! Token: " + token + ", Secret: "
+						+ secret);
+				// mCB.setChecked(true);
+				// mEditGroup.setEnabled(true);
+			}
+		} else {
+			mConsumer = new CommonsHttpOAuthConsumer(Keys.TWITTER_CONSUMER_KEY,
+					Keys.TWITTER_CONSUMER_SECRET, SignatureMethod.HMAC_SHA1);
+
+			SharedPreferences settings = this.getSharedPreferences(PREFS, 0);
+			if (settings.contains(TOKEN_STRING) && settings.contains(SECRET_STRING)) {
+				String token = settings.getString(TOKEN_STRING, "");
+				String secret = settings.getString(SECRET_STRING, "");
+				Log.d(TAG, "From preferences!! Token: " + token + ", Secret: "
+						+ secret);
+				mConsumer.setTokenWithSecret(token, secret);
+			}
+			mProvider = new DefaultOAuthProvider(mConsumer,
+					TWITTER_REQUEST_TOKEN_URL, TWITTER_ACCESS_TOKEN_URL,
+					TWITTER_AUTHORIZE_URL);
+		}
+		
+		getCredentials();
+		mButton.setOnClickListener(this);
+		mCB.setOnCheckedChangeListener(this);
+	}
+
+	@Override
+	protected void onSaveInstanceState(Bundle b) {
+		b.putSerializable("provider", mProvider);
+		b.putSerializable("consumer", mConsumer);
+	}
+
+	@Override
+	protected void onNewIntent(Intent i) {
+
+		Uri uri = i.getData();
+		if (uri != null && CALLBACK_URI.getScheme().equals(uri.getScheme())) {
+			try {
+				String verifier = uri.getQueryParameter(OAuth.OAUTH_VERIFIER);
+				mProvider.retrieveAccessToken(verifier);
+				String token = mConsumer.getToken();
+				String secret = mConsumer.getTokenSecret();
+				this.saveAuthInformation(token, secret);
+				Log.d(TAG, "New OAuth Token: " + token + ", New Secret: "
+						+ secret);
 			} catch (OAuthMessageSignerException e) {
 				e.printStackTrace();
 			} catch (OAuthNotAuthorizedException e) {
@@ -162,24 +152,32 @@ public class BLOA extends Activity implements OnCheckedChangeListener, OnClickLi
 			} catch (OAuthCommunicationException e) {
 				e.printStackTrace();
 			}
-    	} else {
-    		
-    	}
-    }
-    
-    @Override
-    public void onDestroy() {
-    	super.onDestroy();
-		mClient.getConnectionManager().shutdown();
-    }
-    
+		}
+		getCredentials();
+	}
+	
+	private String getUserName(JSONObject credentials) {
+		return credentials.optString("name", "Bad value");
+	}
+
+	private String getLastTweet(JSONObject credentials) {
+		try {
+			JSONObject status = credentials.getJSONObject("status");
+			return status.getString("text");
+		} catch (JSONException e) {
+			e.printStackTrace();
+			return "";
+		}
+	}
+
 	@Override
 	public void onCheckedChanged(CompoundButton arg0, boolean arg1) {
- 		String authUrl = null;
- 		
- 		if(arg1) {
- 	 		try {
-				authUrl = mProvider.retrieveRequestToken(CALLBACK_URI.toString());
+		String authUrl = null;
+
+		if (arg1) {
+			try {
+				authUrl = mProvider.retrieveRequestToken(CALLBACK_URI
+						.toString());
 				Log.d(TAG, "AuthUrl: " + authUrl);
 				Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(authUrl));
 				this.startActivity(i);
@@ -192,101 +190,168 @@ public class BLOA extends Activity implements OnCheckedChangeListener, OnClickLi
 			} catch (OAuthCommunicationException e) {
 				e.printStackTrace();
 			}
- 		} else {
-    	    SharedPreferences settings = this.getSharedPreferences(PREFS, 0);
-    	    SharedPreferences.Editor editor = settings.edit();
-    	    editor.remove(TOKEN_STRING);
-    	    editor.remove(SECRET_STRING);
-    	    editor.commit();
-    	    mUser.setText("");
-        	mButton.setEnabled(false);
-        	mEditor.setEnabled(false);
+		} else {
+			saveAuthInformation(null, null);
+			getCredentials();
 		}
 	}
 
-    public HttpParams getParams() { 
-        //Tweak further as needed for your app 
-        HttpParams params = new BasicHttpParams(); 
-        // set this to false, or else you'll get an Expectation Failed: error 
-        HttpProtocolParams.setUseExpectContinue(params, false); 
-        return params; 
-    }
-    
-    public JSONObject getCredentials() {
-    	try {
-			String response;
-			HttpContext c = new BasicHttpContext();
-			// First let's verify our credentials
-			HttpGet get = new HttpGet("http://twitter.com/account/verify_credentials.json");
+	public HttpParams getParams() {
+		// Tweak further as needed for your app
+		HttpParams params = new BasicHttpParams();
+		// set this to false, or else you'll get an Expectation Failed: error
+		HttpProtocolParams.setUseExpectContinue(params, false);
+		return params;
+	}
 
-			// sign the request to authenticate
-			mConsumer.sign(get);
-			
-	        response = mClient.execute(get, new BasicResponseHandler(), c);
-	        JSONObject respJSO = new JSONObject(response);
-	        Log.d(TAG, "Credentials: " + respJSO.toString(2));
-			
-    		return respJSO;
-    		
-    	} catch (JSONException e) {
-    		e.printStackTrace();
-    	} catch (OAuthMessageSignerException e) {
-			e.printStackTrace();
-		} catch (OAuthExpectationFailedException e) {
-			e.printStackTrace();
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
+
+	public void getCredentials() {
+		new GetCredentialsTask().execute();
+	}
+
+	private class GetCredentialsTask extends AsyncTask<Void, Void, JSONObject> {
+
+		ProgressDialog authDialog;
+
+		@Override
+		protected void onPreExecute() {
+			authDialog = ProgressDialog.show(BLOA.this, getText(R.string.auth_progress_title), getText(R.string.auth_progress_text), true);
 		}
-       	return null;
-    }
-    
-    public JSONObject postTweet() {
-
-    	String response;
-		HttpContext c = new BasicHttpContext();
-		// create a request that requires authentication
-        HttpPost post = new HttpPost("http://twitter.com/statuses/update.json");
-        LinkedList<BasicNameValuePair> out = new LinkedList<BasicNameValuePair>();
-        
-        // 'status' here is the update value you collect from UI
-        out.add(new BasicNameValuePair("status", mEditor.getText().toString()));
-        HttpEntity entity;
-		try {
-			entity = new UrlEncodedFormEntity(out, HTTP.UTF_8);
-			post.setEntity(entity);
-			post.setParams(this.getParams());
-        
-			// sign the request to authenticate
-			mConsumer.sign(post);
 		
-			response = mClient.execute(post, new BasicResponseHandler(), c);
-			mEditor.setText("");
-
-			return new JSONObject(response);
-
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		} catch (OAuthMessageSignerException e) {
-			e.printStackTrace();
-		} catch (OAuthExpectationFailedException e) {
-			e.printStackTrace();
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (JSONException e) {
-			e.printStackTrace();
+		@Override
+		protected JSONObject doInBackground(Void... arg0) {
+			JSONObject jso = null;
+			DefaultHttpClient mClient = new DefaultHttpClient();
+			try {
+				HttpGet get = new HttpGet("http://twitter.com/account/verify_credentials.json");
+				mConsumer.sign(get);
+				String response = mClient.execute(get, new BasicResponseHandler());
+				jso = new JSONObject(response);
+				Log.d(TAG, "Credentials: " + jso.toString(2));
+			} catch (JSONException e) {
+				e.printStackTrace();
+			} catch (OAuthMessageSignerException e) {
+				e.printStackTrace();
+			} catch (OAuthExpectationFailedException e) {
+				e.printStackTrace();
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				mClient.getConnectionManager().shutdown();
+			}
+			return jso;
 		}
-    	return null;
-    }
-    
+		
+		protected void onPostExecute(JSONObject jso) {
+			authDialog.dismiss();
+			if(jso != null) { // authorization succeeded, the json object contains the user information
+				mButton.setEnabled(true);
+				mEditor.setEnabled(true);
+				mUser.setText(getUserName(jso));
+				mDisplay.setText(getLastTweet(jso));
+			} else {
+				mButton.setEnabled(false);
+				mEditor.setEnabled(false);
+				mCB.setChecked(false);
+				mDisplay.setText("");
+			}
+		}
+	}
+	
+	private void saveAuthInformation(String token, String secret) {
+		// null means to clear the old values
+		SharedPreferences settings = BLOA.this.getSharedPreferences(PREFS, 0);
+		SharedPreferences.Editor editor = settings.edit();
+		if(token == null)
+			editor.remove(TOKEN_STRING);
+		else
+			editor.putString(TOKEN_STRING, token);
+		if (secret == null)
+			editor.remove(SECRET_STRING);
+		else
+			editor.putString(SECRET_STRING, secret);
+		editor.commit();
+		
+	}
+	
+	private class PostTask extends AsyncTask<String, Void, JSONObject> {
+
+		ProgressDialog postDialog;
+
+		@Override
+		protected void onPreExecute() {
+			postDialog = ProgressDialog.show(BLOA.this, getText(R.string.tweet_progress_title), getText(R.string.tweet_progress_text), true);
+		}
+		
+		@Override
+		protected JSONObject doInBackground(String... params) {
+
+			DefaultHttpClient mClient = new DefaultHttpClient();
+			// create a request that requires authentication
+			HttpPost post = new HttpPost("http://twitter.com/statuses/update.json");
+			LinkedList<BasicNameValuePair> out = new LinkedList<BasicNameValuePair>();
+			JSONObject jso = null;
+			// 'status' here is the update value you collect from UI
+			out.add(new BasicNameValuePair("status", params[0]));
+			HttpEntity entity;
+			try {
+				entity = new UrlEncodedFormEntity(out, HTTP.UTF_8);
+				post.setEntity(entity);
+				post.setParams(getParams());
+
+				// sign the request to authenticate
+				mConsumer.sign(post);
+
+				String response = mClient.execute(post, new BasicResponseHandler());
+				jso = new JSONObject(response);
+			} catch (UnsupportedEncodingException e) {
+				e.printStackTrace();
+			} catch (OAuthMessageSignerException e) {
+				e.printStackTrace();
+			} catch (OAuthExpectationFailedException e) {
+				e.printStackTrace();
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} catch (JSONException e) {
+				e.printStackTrace();
+			} finally {
+				mClient.getConnectionManager().shutdown();
+			}
+			return jso;
+		}
+		
+		protected void onPostExecute(JSONObject jso) {
+			postDialog.dismiss();
+			if(jso != null) { // authorization succeeded, the json object contains the user information
+				mEditor.setText("");
+				mDisplay.setText(getCurrentTweet(jso));
+			} else {
+				mDisplay.setText(getText(R.string.tweet_error));
+			}
+		}
+		
+	}
+	
+	private String getCurrentTweet(JSONObject response) {
+		return response.optString("text", getString(R.string.bad_value));
+	}
+
+	public void postTweet() {
+		String postString = mEditor.getText().toString();
+		if (postString.length() == 0) {
+			Toast.makeText(this, getText(R.string.tweet_empty),
+					Toast.LENGTH_SHORT).show();
+		} else {
+			new PostTask().execute(postString);
+		}
+	}
+
 	@Override
 	public void onClick(View v) {
-		JSONObject jso = postTweet();
-        String status;
-        status = jso == null ? "Null Value" : jso.optString("text", "Bad Value");
-        mDisplay.setText(status);
+		postTweet();
 	}
 }
