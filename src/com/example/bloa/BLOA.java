2c4ff95d90afeb081209c0066304bf4e4e5a8000
brione <brione2001@gmail.com>
2010-01-27 13:59:27 -0700

    hmm, are we back in sync

--- src/com/example/bloa/BLOA.java
+++ src/com/example/bloa/BLOA.java
@@ -69,11 +69,10 @@ public class BLOA extends Activity implements OnClickListener {
 	
 	ProgressDialog postDialog = null;
 
-	private static final String REQUEST_TOKEN = "request_token";
-	private static final String REQUEST_SECRET = "request_secret";
 	private static final String USER_TOKEN = "user_token";
 	private static final String USER_SECRET = "user_secret";
-
+	private static final String REQUEST_TOKEN = "request_token";
+	private static final String REQUEST_SECRET = "request_secret";
 
 	/** Called when the activity is first created. */
 	@Override
@@ -90,6 +89,7 @@ public class BLOA extends Activity implements OnClickListener {
 		mButton.setOnClickListener(this);
 		mCB.setOnClickListener(this);
 		
+		// We don't need to worry about any saved states: we can reconstruct the state
 		mConsumer = new CommonsHttpOAuthConsumer(
 				Keys.TWITTER_CONSUMER_KEY,
 				Keys.TWITTER_CONSUMER_SECRET, 
@@ -101,40 +101,57 @@ public class BLOA extends Activity implements OnClickListener {
 				TWITTER_ACCESS_TOKEN_URL,
 				TWITTER_AUTHORIZE_URL);
 		
-		// This was the missing key to why it wouldn't run in a standard activity
+		// It turns out this was the missing thing to making standard Activity launch mode work
 		mProvider.setOAuth10a(true);
 
 		SharedPreferences settings = this.getSharedPreferences(PREFS, Context.MODE_PRIVATE);
 		String token;
 		String secret;
 		
+		// We look for saved user keys first. If we find them, we'll test them in onResume()
 		if(settings.contains(USER_TOKEN) && settings.contains(USER_SECRET)) {
 			token = settings.getString(USER_TOKEN, null);
 			secret = settings.getString(USER_SECRET, null);
 			if(!(token == null || secret == null)) {
 				mConsumer.setTokenWithSecret(token, secret);
 			}
-		} else if(settings.contains(REQUEST_TOKEN) && settings.contains(REQUEST_SECRET)) {
+		} else 
+			// Now we see if we saved information from previous request and we're in the process of
+			// coming back with an oauth result. We have to restore the saved request token and secret
+			// so they will be available in onResume() when get the OAUTH response with the verification information
+			if(settings.contains(REQUEST_TOKEN) && settings.contains(REQUEST_SECRET)) {
 			token = settings.getString(REQUEST_TOKEN, null);
 			secret = settings.getString(REQUEST_SECRET, null);
 			if(!(token == null || secret == null)) {
 				mConsumer.setTokenWithSecret(token, secret);
 			}
 		}
+		// You should do this (apparently) whenever you change the consumer in any way, like what we might have
+		// done above. It doesn't hurt to get them reacquainted again. 
 		mProvider.setConsumer(mConsumer);
 	}
 
 	@Override
+	// Right now this will attempt to authenticate every time it is called.
 	protected void onResume() {
 		super.onResume();
+
 		Uri uri = getIntent().getData();
 		if (uri != null && CALLBACK_URI.getScheme().equals(uri.getScheme())) {
 			try {
+				String otoken = uri.getQueryParameter(OAuth.OAUTH_TOKEN);
 				String verifier = uri.getQueryParameter(OAuth.OAUTH_VERIFIER);
+
+				// We send out and save the request token, but the secret is not the same as the verifier
+				// Apparently, the verifier is decoded to get the secret, which is then compared - crafty
+				Assert.assertEquals(otoken, mConsumer.getToken());
+
+				// This is the moment of truth - we could throw here
 				mProvider.retrieveAccessToken(verifier);
-				this.saveAuthInformation(mConsumer.getToken(), mConsumer.getTokenSecret());
-				// clear the request information
+
+				// Clear the saved request information, now that we have a blessed token/secret
 				this.saveRequestInformation(null, null);
+				this.saveAuthInformation(mConsumer.getToken(), mConsumer.getTokenSecret());
 			} catch (OAuthMessageSignerException e) {
 				e.printStackTrace();
 			} catch (OAuthNotAuthorizedException e) {
@@ -163,10 +180,11 @@ public class BLOA extends Activity implements OnClickListener {
 			return getCurrentTweet(status);
 		} catch (JSONException e) {
 			e.printStackTrace();
-			return "";
+			return getString(R.string.tweet_error);
 		}
 	}
 
+	// These parameters are needed to talk to the messaging service
 	public HttpParams getParams() {
 		// Tweak further as needed for your app
 		HttpParams params = new BasicHttpParams();
@@ -175,11 +193,14 @@ public class BLOA extends Activity implements OnClickListener {
 		return params;
 	}
 
+	//----------------------------
+	// This task is run on every onResume(), to make sure the current credentials are valid.
+	// This is probably overkill for a non-educational program
 	private class GetCredentialsTask extends AsyncTask<Void, Void, JSONObject> {
 
 		ProgressDialog authDialog;
 		DefaultHttpClient mClient;
-		
+
 		@Override
 		protected void onPreExecute() {
 			mClient = new DefaultHttpClient();
@@ -217,29 +238,21 @@ public class BLOA extends Activity implements OnClickListener {
 		protected void onPostExecute(JSONObject jso) {
 			mClient.getConnectionManager().shutdown();
 			authDialog.dismiss();
-			if(jso != null) { // authorization succeeded, the json object contains the user information
-				if(!mCB.isChecked())
-					mCB.setChecked(true);
-				mButton.setEnabled(true);
-				mEditor.setEnabled(true);
-				mUser.setText(getUserName(jso));
-				mDisplay.setText(getLastTweet(jso));
-			} else {
-				if(mCB.isEnabled())
-					mCB.setChecked(false);
-				mButton.setEnabled(false);
-				mEditor.setEnabled(false);
-				mCB.setChecked(false);
-				mDisplay.setText("");
-			}
+			mCB.setChecked(jso != null);
+			mButton.setEnabled(jso != null);
+			mEditor.setEnabled(jso != null);
+			mUser.setText(jso != null ? getUserName(jso) : getString(R.string.userhint));
+			mDisplay.setText(jso != null ? getLastTweet(jso) : getString(R.string.userhint));
 		}
 	}
 	
+	//----------------------------
+	// This task posts a message to your message queue on the service.
 	private class PostTask extends AsyncTask<String, Void, JSONObject> {
 
 		ProgressDialog postDialog;
 		DefaultHttpClient mClient;
-		
+
 		@Override
 		protected void onPreExecute() {
 			mClient = new DefaultHttpClient();
@@ -299,6 +312,9 @@ public class BLOA extends Activity implements OnClickListener {
 			if(mCB.isChecked()) {
 				try {
 					String authUrl = mProvider.retrieveRequestToken(CALLBACK_URI.toString());
+					Log.d(TAG, "onClick() - AuthUrl: " + authUrl);
+					Log.d(TAG, "onClick() - Request: " + mConsumer.getToken());
+					Log.d(TAG, "onClick() - Secret: " + mConsumer.getTokenSecret());
 					saveRequestInformation(mConsumer.getToken(), mConsumer.getTokenSecret());
 					Intent i = new Intent(Intent.ACTION_VIEW, Uri.parse(authUrl));
 					this.startActivity(i);
@@ -318,6 +334,7 @@ public class BLOA extends Activity implements OnClickListener {
 				mCB.setChecked(false);
 				mDisplay.setText("");
 			}
+			mCB.setChecked(false); // the oauth callback will set it to the proper state
 		} else if(mButton.equals(v)) {
 			String postString = mEditor.getText().toString();
 			if (postString.length() == 0) {
@@ -331,7 +348,7 @@ public class BLOA extends Activity implements OnClickListener {
 
 	private void saveRequestInformation(String token, String secret) {
 		// null means to clear the old values
-		SharedPreferences settings = BLOA.this.getSharedPreferences(PREFS, 0);
+		SharedPreferences settings = BLOA.this.getSharedPreferences(PREFS, Context.MODE_PRIVATE);
 		SharedPreferences.Editor editor = settings.edit();
 		if(token == null) {
 			editor.remove(REQUEST_TOKEN);
@@ -339,7 +356,7 @@ public class BLOA extends Activity implements OnClickListener {
 		}
 		else {
 			editor.putString(REQUEST_TOKEN, token);
-			Log.d(TAG, "Saving Request token: " + token);
+			Log.d(TAG, "Saving Request Token: " + token);
 		}
 		if (secret == null) {
 			editor.remove(REQUEST_SECRET);
@@ -355,7 +372,7 @@ public class BLOA extends Activity implements OnClickListener {
 	
 	private void saveAuthInformation(String token, String secret) {
 		// null means to clear the old values
-		SharedPreferences settings = BLOA.this.getSharedPreferences(PREFS, 0);
+		SharedPreferences settings = BLOA.this.getSharedPreferences(PREFS, Context.MODE_PRIVATE);
 		SharedPreferences.Editor editor = settings.edit();
 		if(token == null) {
 			editor.remove(USER_TOKEN);
