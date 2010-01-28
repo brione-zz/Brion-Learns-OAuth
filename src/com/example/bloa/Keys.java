6a941b75585665ff0ceede2a43932fb20bd427d4
Brion Emde <brione2001@gmail.com>
2010-01-27 18:11:42 -0700

    I inadvertently put my actual token and secret in here. So I created new tokens (I'm the only user) and those won't work anymore.

--- src/com/example/bloa/Keys.java
+++ src/com/example/bloa/Keys.java
@@ -1,6 +1,6 @@
 package com.example.bloa;
 
 public class Keys {
-	public static final String TWITTER_CONSUMER_KEY = "6uWmSQRhKlQi7xNtj0w";
-	public static final String TWITTER_CONSUMER_SECRET = "Ilm3c0D7BV6GkTMpcPLMwFtMyeUYepa3hoIBqZUL9w";
+	public static final String TWITTER_CONSUMER_KEY = "Your Twitter Application Token";
+	public static final String TWITTER_CONSUMER_SECRET = "Your Twitter Application Secret";
 }
