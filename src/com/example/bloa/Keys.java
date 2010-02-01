447ceec9eda67ff6285b219acadc101979858a7f
Brion Emde <brione2001@gmail.com>
2010-02-01 13:54:04 -0700

    Put my key and secret in the public repo again...

--- src/com/example/bloa/Keys.java
+++ src/com/example/bloa/Keys.java
@@ -1,6 +1,6 @@
 package com.example.bloa;
 
 public class Keys {
-	public static final String TWITTER_CONSUMER_KEY = "QjBnTzlnqF218zJ2wzbiug";
-	public static final String TWITTER_CONSUMER_SECRET = "IePYFdLnmTJCitixe66VZ8WtVv7RbeazoWmZeg1e1RM";
+	public static final String TWITTER_CONSUMER_KEY = "Your Key";
+	public static final String TWITTER_CONSUMER_SECRET = "Your Secret";
 }
