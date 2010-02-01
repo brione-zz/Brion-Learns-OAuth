36d1139c50ce400441ac68cc0c07f467e07b8250
Brion Emde <brione2001@gmail.com>
2010-02-01 14:38:33 -0700

    turned the Keys class into a subclass of MyKeys, which is not archived. That way I won't keep giving away my token and key and having to reset them

--- src/com/example/bloa/Keys.java
+++ src/com/example/bloa/Keys.java
@@ -1,6 +1,8 @@
 package com.example.bloa;
 
-public class Keys {
-	public static final String TWITTER_CONSUMER_KEY = "Your Twitter Application Token";
-	public static final String TWITTER_CONSUMER_SECRET = "Your Twitter Application Secret";
+// You either come up with your own MyKeys class or you take out the extension and uncomment the values here
+// with your own token and key. Let's see how this works as far as me not giving away my stuff all the time!
+public class Keys extends MyKeys {
+	// public static final String TWITTER_CONSUMER_KEY = "Your Twitter Application Token";
+	// public static final String TWITTER_CONSUMER_SECRET = "Your Twitter Application Secret";
 }
