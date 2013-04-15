eedf540ab4bdcfa553f9cb6ef54ab79ddfc1a153
Brion Emde <brione2001@gmail.com>
2013-04-14 23:57:35 -0700

    Added Apache License headers throughout the main app

--- src/com/example/bloa/data/UserStatusRecords.java
+++ src/com/example/bloa/data/UserStatusRecords.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2010 - Brion Noble Emde
+ * Copyright 2013 - Brion Noble Emde
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
