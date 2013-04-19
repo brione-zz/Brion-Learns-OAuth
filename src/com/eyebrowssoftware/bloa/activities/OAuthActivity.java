3bb2231d492a5b0a9905e06a08049baa4b005a53
Brion Emde <brione2001@gmail.com>
2013-04-18 21:38:35 -0700

    Interim commit

--- src/com/eyebrowssoftware/bloa/activities/OAuthActivity.java
+++ src/com/eyebrowssoftware/bloa/activities/OAuthActivity.java
@@ -32,7 +32,6 @@ import org.json.JSONObject;
 
 import android.accounts.Account;
 import android.accounts.AccountAuthenticatorActivity;
-import android.accounts.AccountAuthenticatorResponse;
 import android.accounts.AccountManager;
 import android.content.ContentResolver;
 import android.content.ContentValues;
@@ -59,7 +58,6 @@ public class OAuthActivity extends AccountAuthenticatorActivity {
    private  OAuthConsumer mConsumer;
    private  Intent mIntent;
    private  App mApp;
-   private  AccountAuthenticatorResponse mResponse;
    private AccountManager mAccountManager;
    private Boolean mConfirmCredentials = false;
    private Boolean mRequestNewAccount = false;
@@ -79,46 +77,8 @@ public class OAuthActivity extends AccountAuthenticatorActivity {
         mConsumer = mApp.getOAuthConsumer();
 
         mIntent = this.getIntent();
-        mResponse = mIntent.getParcelableExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE);
+        mIntent.getParcelableExtra(AccountManager.KEY_ACCOUNT_AUTHENTICATOR_RESPONSE);
         (new RetrieveRequestTokenTask()).execute(new Void[0]);
-    }
-
-    @Override
-    public void onNewIntent(Intent intent) {
-        super.onNewIntent(intent);
-
-        if (Log.isLoggable(TAG, Log.DEBUG)) {
-            Log.d(TAG, "onNewIntent() called");
-        }
-        Uri uri = intent.getData();
-        if (uri != null) {
-            // Get the stuff we saved in the async task so we can confirm that it all matches up
-
-            String token = mSettings.getString(Constants.REQUEST_TOKEN, null);
-            String secret = mSettings.getString(Constants.REQUEST_SECRET, null);
-
-            // Intent i = new Intent(this, BloaActivity.class); // Currently how we get back to the main activity
-
-            if (token == null || secret == null) {
-                throw new IllegalStateException("We should have saved!");
-            }
-            String otoken = uri.getQueryParameter(OAuth.OAUTH_TOKEN);
-            if (otoken != null) {
-                // This is a sanity check which should never fail - hence the assertion
-                Assert.assertEquals(otoken, mConsumer.getToken());
-
-                // We send out and save the request token, but the secret is not the same as the verifier
-                // Apparently, the verifier is decoded to get the secret, which is then compared - crafty
-                String verifier = uri.getQueryParameter(OAuth.OAUTH_VERIFIER);
-
-                // We do this in a task now or get an automatic crash
-                (new RetrieveAccessTokenTask()).execute(verifier);
-            } else {
-                String denied = uri.getQueryParameter("denied");
-                Log.e(TAG, "Access denied or canceled. Token returned is: " + denied);
-                finish();
-            }
-        }
     }
 
     // This is new and required - we can't be decoding the tokens on the UI thread anymore
@@ -152,50 +112,63 @@ public class OAuthActivity extends AccountAuthenticatorActivity {
         }
     }
 
-    private ContentValues parseVerifyUserJSONObject(JSONObject object) throws JSONException {
-        ContentValues values = new ContentValues();
-        values.put(UserStatusRecord.USER_NAME, object.getString("name"));
-        values.put(UserStatusRecord.RECORD_ID, object.getInt("id_str"));
-        values.put(UserStatusRecord.USER_CREATED_DATE, object.getString("created_at"));
-        JSONObject status = object.getJSONObject("status");
-        values.put(UserStatusRecord.USER_TEXT, status.getString("text"));
-        return values;
+    // This is the callback from the browser-based authentication
+    @Override
+    public void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+
+        if (Log.isLoggable(TAG, Log.DEBUG)) {
+            Log.d(TAG, "onNewIntent() called");
+        }
+        Uri uri = intent.getData();
+        Assert.assertNotNull(uri);
+        String otoken = uri.getQueryParameter(OAuth.OAUTH_TOKEN);
+        if (otoken != null) {
+            // This is a sanity check which should never fail - hence the assertion
+            Assert.assertEquals(otoken, mConsumer.getToken());
+            /*
+             * We have to do this in a thread now or get an automatic crash
+             * because it involves hidden network operations
+             */
+            String verifier = uri.getQueryParameter(OAuth.OAUTH_VERIFIER);
+            (new RetrieveAccessTokenTask()).execute(verifier);
+        }
     }
 
+    // This is new and required - we can't be decoding the tokens on the UI thread anymore
+    private class RetrieveAccessTokenTask extends AsyncTask<String, Void, OAuthConsumer> {
 
-    //----------------------------
-    // This task is run on every onResume(), to make sure the current credentials are valid.
-    // This is probably overkill for a non-educational program
-    class GetCredentialsTask extends AsyncTask<Void, Void, Boolean> {
-
-        HttpClient mClient = App.getHttpClient();
         @Override
-        protected Boolean doInBackground(Void... arg0) {
-            JSONObject jso = null;
-            HttpGet get = new HttpGet(Constants.VERIFY_URL_STRING);
+        protected OAuthConsumer doInBackground(String... verifiers) {
             try {
-                mConsumer.sign(get);
-                String response = mClient.execute(get, new BasicResponseHandler());
-                if (response != null) {
-                    jso = new JSONObject(response);
-                    App.makeNewUserStatusRecord(OAuthActivity.this.getContentResolver(), parseVerifyUserJSONObject(jso));
-                    return true;
-                } else {
-                    Log.e(TAG, "PostTask: null response text");
-                    throw new IllegalStateException("Expected some text in the Http response");
-                }
-            } catch (Exception e) {
-                // Expected if we don't have the proper credentials saved away
+                // This is the moment of truth - we could throw here
+                mProvider.retrieveAccessToken(mConsumer, verifiers[0]);
+                return mConsumer;
+            } catch (OAuthMessageSignerException e) {
+                e.printStackTrace();
+            } catch (OAuthNotAuthorizedException e) {
+                e.printStackTrace();
+            } catch (OAuthExpectationFailedException e) {
+                e.printStackTrace();
+            } catch (OAuthCommunicationException e) {
+                e.printStackTrace();
             }
-            return false;
+            return null;
         }
 
-        // This is in the UI thread, so we can mess with the UI
         @Override
-        protected void onPostExecute(Boolean loggedIn) {
-            super.onPostExecute(loggedIn);
-       }
+        protected void onPostExecute(OAuthConsumer consumer) {
+            super.onPostExecute(consumer);
+
+            OAuthActivity.this.finish();
+            if (consumer != null) {
+                onAuthenticationResult(consumer.getToken(), consumer.getTokenSecret());
+            } else {
+                onAuthenticationCanceled();
+            }
+        }
     }
+
 
     /**
      * Called when response is received from the server for confirm credentials
@@ -206,10 +179,22 @@ public class OAuthActivity extends AccountAuthenticatorActivity {
      */
     private void finishConfirmCredentials(String token, String secret) {
         Log.i(TAG, "finishConfirmCredentials()");
-        final Account account = new Account(token, Constants.ACCOUNT_TYPE);
-        mAccountManager.setPassword(account, secret);
+        final Account account = new Account(mConsumer.getToken(), Constants.ACCOUNT_TYPE);
+        if (mRequestNewAccount) {
+            mAccountManager.addAccountExplicitly(account, mConsumer.getTokenSecret(), null);
+            // Set contacts sync for this account.
+            ContentResolver.setSyncAutomatically(account, BloaProvider.AUTHORITY, true);
+        } else {
+            mAccountManager.setPassword(account, mConsumer.getTokenSecret());
+        }
+        Bundle result = new Bundle();
+        result.putString(Constants.PARAM_USERNAME, mConsumer.getToken());
+        result.putString(Constants.PARAM_PASSWORD, mConsumer.getTokenSecret());
+        OAuthActivity.this.setAccountAuthenticatorResult(result);
+        App.saveRequestInformation(mSettings, null, null);
         final Intent intent = new Intent();
-        intent.putExtra(AccountManager.KEY_BOOLEAN_RESULT, RESULT_OK);
+        intent.putExtra(AccountManager.KEY_ACCOUNT_NAME, mConsumer.getToken());
+        intent.putExtra(AccountManager.KEY_ACCOUNT_TYPE, Constants.ACCOUNT_TYPE);
         setAccountAuthenticatorResult(intent.getExtras());
         setResult(RESULT_OK, intent);
         finish();
@@ -238,6 +223,7 @@ public class OAuthActivity extends AccountAuthenticatorActivity {
         final Intent intent = new Intent();
         intent.putExtra(AccountManager.KEY_ACCOUNT_NAME, token);
         intent.putExtra(AccountManager.KEY_ACCOUNT_TYPE, Constants.ACCOUNT_TYPE);
+        intent.putExtra(AccountManager.KEY_AUTH_TOKEN_LABEL, secret);
         setAccountAuthenticatorResult(intent.getExtras());
         setResult(RESULT_OK, intent);
         finish();
@@ -254,62 +240,4 @@ public class OAuthActivity extends AccountAuthenticatorActivity {
     private void onAuthenticationCanceled() {
 
     }
-
-
-
-    // This is new and required - we can't be decoding the tokens on the UI thread anymore
-    private class RetrieveAccessTokenTask extends AsyncTask<String, Void, OAuthConsumer> {
-
-        @Override
-        protected OAuthConsumer doInBackground(String... verifiers) {
-            try {
-                // This is the moment of truth - we could throw here
-                mProvider.retrieveAccessToken(mConsumer, verifiers[0]);
-                return mConsumer;
-            } catch (OAuthMessageSignerException e) {
-                e.printStackTrace();
-            } catch (OAuthNotAuthorizedException e) {
-                e.printStackTrace();
-            } catch (OAuthExpectationFailedException e) {
-                e.printStackTrace();
-            } catch (OAuthCommunicationException e) {
-                e.printStackTrace();
-            }
-            OAuthActivity.this.finish();
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(OAuthConsumer consumer) {
-            super.onPostExecute(consumer);
-
-            if (consumer != null) {
-                onAuthenticationResult(consumer.getToken(), consumer.getTokenSecret());
-
-
-
-                final Account account = new Account(mConsumer.getToken(), Constants.ACCOUNT_TYPE);
-                if (mRequestNewAccount) {
-                    mAccountManager.addAccountExplicitly(account, mConsumer.getTokenSecret(), null);
-                    // Set contacts sync for this account.
-                    ContentResolver.setSyncAutomatically(account, BloaProvider.AUTHORITY, true);
-                } else {
-                    mAccountManager.setPassword(account, mConsumer.getTokenSecret());
-                }
-                Bundle result = new Bundle();
-                result.putString(Constants.PARAM_USERNAME, mConsumer.getToken());
-                result.putString(Constants.PARAM_PASSWORD, mConsumer.getTokenSecret());
-                OAuthActivity.this.setAccountAuthenticatorResult(result);
-                App.saveRequestInformation(mSettings, null, null);
-                final Intent intent = new Intent();
-                intent.putExtra(AccountManager.KEY_ACCOUNT_NAME, mConsumer.getToken());
-                intent.putExtra(AccountManager.KEY_ACCOUNT_TYPE, Constants.ACCOUNT_TYPE);
-                setAccountAuthenticatorResult(intent.getExtras());
-                setResult(RESULT_OK, intent);
-                finish();
-            }
-        }
-    }
-
-
 }
